2cdf4324742e3fae4094fae4a4eb4b6b9ba83ac9
Geoffrey Donnell <geoffwd@uw.edu>
2021-04-25 15:43:03 -0700

    yay! My program works! I misread the data requirements but was able to make it handle lists of lists!

--- mailroom.py
+++ mailroom.py
@@ -8,40 +8,36 @@ You can store that data structure in the global namespace.
 The script should prompt the user (you) to choose from a menu of 3 actions:
 “Send a Thank You”, “Create a Report” or “quit”.
 '''
-'''Global variables, I've seperated the data so that it feeds into one variable. Trying to predict that I will have to
-pull directly from a spreadsheet soon thus eliminating the variables donor_list, donor_amount, and donor_freq
 
-'''
-donor_list = ('Tony Stark',
+donor_list = ['Tony Stark',
               'Steven Rogers',
               'Bruce Wayne',
               'Clark Kent',
-              'Geoffrey Donnell')
-donor_amount = [0, 0, 0, 0, 0]
-donor_freq = [2, 3, 1, 3, 365]
-
+              'Geoffrey Donnell']
+donation_history = [[10000], [200, 300], [1000, 2000, 3000], [100], [23000]]
 
 def menu():
     # This function will be the one that runs everytime and will point to other functions to compelete
-    # the three taks. (1) send a thank you (2) generate a report (3) or quit.
+    # the three tasks. (1) Generate a report (2) generate a report (3) or quit.
 
     print('Welcome to the Automated Mail Room System (AMRS).\n'
           'Please select from the three options below \n'
           'by entering the value contained in the ()\n'
-          '(1) Send a Thank You\n'
-          '(2) Create a Report\n'
+          '(1) Generate a Report\n'
+          '(2) Send a Thank You\n'
           '(3) Quit the program')
     choice = (input('What would you like to do? Enter value: '))
 
     while choice not in ['1', '2', '3']:
-        choice = (input('Please enter a valid choice: 1, 2, or 3: '))
+        choice = (input('Invalid value. Please enter a valid choice: 1, 2, or 3: '))
 
     if choice == '1':
-        letter_options()
-    if choice =='2':
         generate_report()
+    if choice =='2':
+        letter_options()
     if choice == '3':
         print("\n Thanks for using AMRS, Goodbye!")
+        quit()
     return
 
 def letter_options():
@@ -53,51 +49,63 @@ def letter_options():
             letter_options()
         if donor_name in donor_list:
             print('You have entered an existing donor')
-            donation_amount = int(input('How much is the additional donation? Please enter a value: '))
+            new_donation = float(input('How much is the additional donation? Please enter a value: '))
             # Start of loop to determine where in the list the donor is and their corresponding amount and freq
-            for index in range(0,len(donor_list)):
+            for index in range(0, int(len(donor_list))):
                 if donor_list[index] == donor_name:
-                    pointer_index = index
-            # Adding the additional donation to the data base
-            donor_amount[pointer_index] = donor_amount[pointer_index] + donation_amount
-            donor_freq[pointer_index] = donor_freq[pointer_index] + 1
-            draft_email(donor_name[pointer_index],donor_amount[pointer_index])
-            menu()
+                    # Adding the additional donation to the data base
+                    donation_history[index].append(new_donation)
+            # Calls function to generate email
+            draft_email(donor_name, new_donation)
         else:
-            donation_amount = input(f'You have entered a new donor named {donor_name}, enter an amount in th'
-                                    f'the form 0.00: ')
-            print(f'Thank for your donation {donor_name} of ${donation_amount}')
-
-            print()
-            menu()
-        break
-        pass
+            new_donation = input(f'You have entered a new donor named {donor_name}, '
+                                    f'enter an amount in the form 0.00: ')
+            donor_list.append((str(donor_name)))
+            donation_history.append([float(new_donation)])
+            draft_email(donor_name, new_donation)
 
 def draft_email(donor_name, donor_amount):
     print(f'\nEmail to: {donor_name.replace(" ","")}@gmail.com')
-    print('\n')
     print('Subject:Donation Received')
-    print(f'\nThank you {donor_name} for your generous donation of {donor_amount}! \n'
-          f'We greatly appreciate your support to our cause. \n \n'
-          f'Best regards, \n'
-          f'Donation Society of Money \n')
+    print(f'\nThank you {donor_name} for your generous donation of ${donor_amount}! \n'
+          f'We greatly appreciate your support to our cause. \n'
+          f'Best regards,\n'
+          f'Staff Member\n \n')
+    menu()
     pass
 
 def generate_report():
+    #Need to delcare empty lists to store values that will be calculated
+    no_donors = int(len(donor_list))
+    donation_freq = [0] * no_donors
+    donation_total = [0] * no_donors
+    donation_average = [0]*no_donors
+    # Finds the average and number of donations
+    for index in range(0, no_donors):
+        donation_total[index] = sum(donation_history[index])
+        donation_freq[index] = len(donation_history[index])
+        donation_average[index] = donation_total[index]/donation_freq[index]
+
     #Formating and Printing for Header
     header = ['Donor Name','Total Given','Number Gifts','Average Gifts']
     header_format = '{:18}|{:16}|{:16}|{:20}|'
+    print('\n')
     print(header_format.format(*header))
     print('-'*int(len(header_format.format(*header)))) #dividing line for the report
 
     #Formating and Printing for Donor List
     donor_format = '{:18}|${:15.2f}|{:16}|${:19.2f}|'
 
-    for step in range(0,5):
-        print(donor_format.format(donor_list[step], donor_amount[step],
-                                  donor_freq[step], (donor_amount[step] / donor_freq[step])))
-    menu() #after report is generated we will go back to the menu
+    for step in range(0,no_donors):
+        print(donor_format.format(donor_list[step],
+                                  donation_total[step],
+                                  int(donation_freq[step]),
+                                  float(donation_average[step])))
+    print('\n') # print a line for spacing between the report and menu
+    menu() # after report is generated we will go back to the menu
     pass
 
+if __name__ == '__main__':
+    menu()
 
-menu()
+
